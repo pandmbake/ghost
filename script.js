0b85168a4c5c7e8b5cf3a8b5e4a1010b97156527
Paul Bakeman <pandmbake@comcast.net>
2022-07-09 19:21:38 -0400

    add javascript

--- script.js
+++ script.js
@@ -1,14 +1,25 @@
-var y = document.getElementById("slide-in-blurred-bottom");
+var x = document.getElementById("slide-in-blurred-bottom");
+var y = document.getElementById("crazy");
+var a = document.getElementById("ghostBackground");
+var b = document.getElementById("scream");
 
-y.addEventListener('click', function click () {
-   
-    if (y.style.width === "500px") {
-        y.style.width = "";
+function change () {
+    if (x.getAttribute('src') === "245787032022212.png") {
+        Image.src = "7fe4622378320f5bd8d594facffc6b58.gif";
+        x.style.visibility = "hidden";
+        y.style.visibility = "visible";
+        a.style.visibility = "hidden";
     } else {
-        y.style.width = "500px";
-    }
-})
+        Image.src = "24578703202221.png";
+        x.style.visibility = "visible";
+        y.style.visibility = "hidden";
+        a.style.visibility = "visible";
+    
+    } 
+}    
+
+
+
 
     
-      
     

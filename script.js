4edb6cad8f738ab44116a7f07a7d22e40a7c7d18
Paul Bakeman <pandmbake@comcast.net>
2022-07-09 17:22:30 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+var y = document.getElementById("slide-in-blurred-bottom");
+
+y.addEventListener('click', function click () {
+   
+    if (y.style.width === "500px") {
+        y.style.width = "";
+    } else {
+        y.style.width = "500px";
+    }
+})
+
+    
+      
+    

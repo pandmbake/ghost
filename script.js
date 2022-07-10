1569cd9abb65b7ac851ef79f52222b5ee58e2862
Paul Bakeman <pandmbake@comcast.net>
2022-07-09 20:03:18 -0400

    new

--- script.js
+++ script.js
@@ -2,22 +2,25 @@ var x = document.getElementById("slide-in-blurred-bottom");
 var y = document.getElementById("crazy");
 var a = document.getElementById("ghostBackground");
 var b = document.getElementById("scream");
+var c = document.getElementById("sound");
 
 function change () {
-    if (x.getAttribute('src') === "245787032022212.png") {
-        Image.src = "7fe4622378320f5bd8d594facffc6b58.gif";
-        x.style.visibility = "hidden";
-        y.style.visibility = "visible";
-        a.style.visibility = "hidden";
-        
-    } else {
-        Image.src = "24578703202221.png";
-        x.style.visibility = "visible";
-        y.style.visibility = "hidden";
-        a.style.visibility = "visible";
-        
-    } 
+    x.style.visibility = "hidden";
+    y.style.visibility = "visible";
+    a.style.visibility = "hidden";
+    c.autoplay = false;
+    b.autoplay = true;
+    
+} 
+
+function changeBack() {
+    x.style.visibility = "visible";
+    y.style.visibility = "hidden";
+    a.style.visibility = "visible";
+    c.autoplay = true;
+    b.autoplay = false;
 }    
+    
 
 
 
